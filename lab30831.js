89bb25dea39499fc9415a383d2f1a9a05eed55a6
RomanFisc <romanfisc@gmail.com>
2024-05-23 13:21:03 -0400

    finished part 1 fizzbuzz

---
+++ lab30831.js
@@ -0,0 +1,19 @@
+// Part 1 Fizz Buzz
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+
+for (let i = 0; i < 101; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fizzbuzz")
+  } else if (i % 5 == 0) {
+    console.log ("buzz")
+  } else if (i % 3 == 0) {
+    console.log("fizz")
+  } else {
+    console.log(i)
+  }
+}
+
